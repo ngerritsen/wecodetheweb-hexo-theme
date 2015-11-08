8766ed3058b3ccdafe426b48d4f9a52ad89fd3e5
Niels Gerritsen <n.gerritsen@coolblue.nl>
2015-11-08 15:39:45 +0100

    added styling

--- source/js/main.js
+++ source/js/main.js
@@ -4,8 +4,66 @@ $().ready(function () {
 
   // Dark switcher
   $('#js-dark-switch').click(function () {
-    $('body').toggleClass('dark');
+    body.toggleClass('dark');
   });
+
+  // Text size switcher
+  var smallBtn  = $('.tools--font-small');
+  var mediumBtn = $('.tools--font-medium');
+  var largeBtn = $('.tools--font-large');
+
+  function applyTextSize (size) {
+    storeTextSize(size);
+    resetTextSize();
+
+    switch (size) {
+      case 'small':
+        smallBtn.addClass('is-active');
+        body.addClass('alt-small');
+        break;
+      case 'medium':
+        mediumBtn.addClass('is-active');
+        break;
+      case 'large':
+        largeBtn.addClass('is-active');
+        body.addClass('alt-large');
+        break;
+    }
+  }
+
+  function storeTextSize (size) {
+    if (Storage) {
+      localStorage.setItem('textSize', size);
+    }
+  }
+
+  function resetTextSize () {
+    smallBtn.removeClass('is-active');
+    mediumBtn.removeClass('is-active');
+    largeBtn.removeClass('is-active');
+
+    body.removeClass('alt-small');
+    body.removeClass('alt-large');
+  }
+
+  function initTextSize () {
+    smallBtn.click(function () {
+      applyTextSize('small');
+    });
+    mediumBtn.click(function () {
+      applyTextSize('medium');
+    });
+    largeBtn.click(function () {
+      applyTextSize('large');
+    });
+
+    if (Storage) {
+      var size = localStorage.getItem('textSize');
+      applyTextSize(size);
+    }
+  }
+
+  initTextSize();
 
   // Header hider
   var headerHight = 70;
